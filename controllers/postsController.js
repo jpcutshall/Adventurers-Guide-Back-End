b555241609c6900271b65009a22599c5defc7413
jpcutshall <jpcutshall@gmail.com>
2020-12-10 23:39:52 -0500

    added update route for posts

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -8,6 +8,21 @@ posts.get('/all', async (req, res) => {
     res.json(posts)
 })
 
+posts.put('/edit/:id', auth, async (req, res) => {
+    try {
+        let { name, about, gtk, camping, background, lat, long, tags} = req.body
+        if (!name || !about) {
+            return res.status(400).json({msg: "Please atleast fill out the name and about field"})
+        }
+        const updatedPost = { name, about, gtk, camping, background, lat, long, tags}
+        const updatePostRes = await Post.findByIdAndUpdate(req.params.id, updatedPost)
+        res.json(updatePostRes) 
+
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
 posts.post('/', auth, async (req, res) => {
 
     try {
@@ -15,6 +30,9 @@ posts.post('/', auth, async (req, res) => {
         
         if (!name || !about) {
             return res.status(400).json({msg: "Please atleast fill out the name and about field"})
+        }
+        if (!lat || !long) {
+            return res.status(400).json({msg: "Please Click on the map where the Location is Relative or Specific"})
         }
         const existingPost = await Post.findOne({ name: name })
         if (existingPost) {
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -76,7 +76,7 @@ users.post('/login', async (req, res) => {
 
 		
 		// JAV WEB TOKEN
-		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h'})
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: 60 * 60})
 		console.log(`Logged in! ${token}`);
 		res.json({
 			token,
